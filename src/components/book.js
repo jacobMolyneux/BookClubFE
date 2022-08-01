1dae2e8491dc416f14f02f73d925ddb95abfa52f
Jacob Molyneux <jacobmolyneux@Jacobs-MacBook-Pro.local>
2022-08-01 03:05:11 -0400

    changed style for book

--- src/components/book.js
+++ src/components/book.js
@@ -1,15 +1,30 @@
 import Card from 'react-bootstrap/Card'
 import {useState} from 'react'
-const Book = () => {
-    
+const Book = (props) => {
+   let [showReview, setShowReview] = useState(false) 
+   const showReviewSide = () => setShowReview(true)
+   const showTitleSide = () => setShowReview(false)
+   
+   if (showReview == false){
     return(
-        <Card className = 'm-3'>
-            <Card.Header>Book Title</Card.Header>
-            <Card.Body className = 'd-flex align-center'>
-                <p>Book title</p>
+        <Card className = 'm-3 border-primary p-3' style = {{minHeight: '200px', minWidth: '150px'}} onClick = {showReviewSide}>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Body className = 'd-column align-center'>
+                <Card.Text>{props.author}</Card.Text>
             </Card.Body>
-            <Card.Footer>Book Author</Card.Footer>
         </Card>
     )
+   }
+   else if(showReview == true){
+       return(
+           <Card className ='m-3 border-primary p-3' style = {{minHeight: '200px', minWidth: '150px'}} onClick = {showTitleSide}>
+               <Card.Title>{props.title}</Card.Title>
+               <Card.Body className = 'd-column align-center'>
+                   <p>{props.review}</p>
+                   <p><em>Rating:</em>{props.rating}</p>
+               </Card.Body>
+           </Card>
+       )
+   }
 }
 export {Book}
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,6 +3,9 @@ import Stack from 'react-bootstrap/Stack'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import booksImage from '../images/antique-books-on-shelves.jpg'
+
+import Image from 'react-bootstrap/Image'
 import {useState} from 'react'
 const Homepage = () => {
     let [showForm, setShowForm] = useState(false);
@@ -10,9 +13,46 @@ const Homepage = () => {
     let [bookAuthor, setBookAuthor] = useState('')
     let [bookReview, setBookReview] = useState('')
     let [bookRating, setBookRating] = useState(null)
+    let [library, setLibrary] = useState([
+        {
+            "Title": "Harry Potter",
+            "Author": 'JK Rowling',
+            "Review": "this is a book review",
+            "Rating": '5'
+        },
+        {
+            "Title": "Harry Potter",
+            "Author": 'JK Rowling',
+            "Review": "this is a book review",
+            "Rating": '5'
+        },
+        {
+            "Title": "Harry Potter",
+            "Author": 'JK Rowling',
+            "Review": "this is a book review",
+            "Rating": '5'
+        },
+        {
+            "Title": "Harry Potter",
+            "Author": 'JK Rowling',
+            "Review": "this is a book review",
+            "Rating": '5'
+        },
+    ])
     
     const handleClose = () => setShowForm(false)
     const handleShow = () => setShowForm(true)
+
+    const add_a_book = () => {
+        let bookData = [{
+            'Title': bookTitle,
+            'Author': bookAuthor,
+            'Review': bookReview,
+            'Rating': bookRating
+        }]
+        library.push(bookData)
+
+    }
     return(
         <div>
             <Modal show ={showForm}
@@ -26,11 +66,11 @@ const Homepage = () => {
                     <Form>
                         <Form.Group>
                             <Form.Label>Author</Form.Label>
-                            <Form.Control type ='text' placeholder = 'Author'></Form.Control>
+                            <Form.Control type ='text' placeholder = 'Author' onChange = {(e) => {setBookAuthor(e.target.value)}}></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Title</Form.Label>
-                            <Form.Control type ='text' placeholder = 'Title'></Form.Control>
+                            <Form.Control type ='text' placeholder = 'Title' onChange = {(e) => {setBookTitle(e.target.value)}}></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Rating</Form.Label>
@@ -43,23 +83,32 @@ const Homepage = () => {
                             </Form.Select>
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Book Review</Form.Label>
+                            <Form.Label onChange = {(e) => {setBookReview(e.target.value)}}>Book Review</Form.Label>
                             <Form.Control placeholder = 'What did you think?' as = 'textarea'></Form.Control>
                         </Form.Group>
                          </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant='secondary' onClick = {handleClose}>close</Button>
-                    <Button variant='primary'>Okay</Button>
+                    <Button variant='primary' onClick = {add_a_book}>Okay</Button>
                 </Modal.Footer>
                 
             </Modal>
             <div>
-            <h1>Your Library</h1>
+                
+            <div className = 'border-bottom d-flex justify-content-between p-3'>
+                <h1>Your Library</h1>
+                <Button variant = 'light'>Filter</Button>
+            </div>
             <Button onClick = {handleShow}>Add a Book</Button>
             </div>
             <Stack direction="horizontal" gap={3}>       
             <Book/>
+            {library.map((book) => {
+                return(
+                    <Book title = {book.Title} author = {book.Author} review = {book.Review} rating = {book.Rating}/>
+                )
+            })}
             
             </Stack>
             
